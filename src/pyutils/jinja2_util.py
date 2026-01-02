8136f1276fb12749ff1a91e2258477ed990ac4f4
mirekfoo <mirekfoo@users.noreply.github.com>
2026-01-02 15:33:06 +0100

    jinja2_util added relative_to function

--- src/pyutils/jinja2_util.py
+++ src/pyutils/jinja2_util.py
@@ -5,6 +5,41 @@ from jinja2 import Template, Environment
 
 jinja2_env = None
 
+def relative_to(master_path: str, slave_path: str) -> str:
+    """
+    Calculate the relative path from master_path to slave_path.
+
+    Args:
+        master_path: The base directory path.
+        slave_path: The target path to make relative.
+
+    Returns:
+        The relative path from master_path to slave_path.
+    """
+    
+    rel_path =  Path(slave_path).relative_to(master_path, walk_up=True)
+    return rel_path
+
+def setupJinja2Env(env: Environment = None) -> Environment:
+    """
+    Setup the Jinja2 environment.
+
+    Args:
+        env: The environment to setup.
+
+    Returns:
+        The setup environment.
+    """
+
+    global jinja2_env
+    
+    if env is None:
+        if jinja2_env is None:
+            jinja2_env = Environment()
+            jinja2_env.globals["relative_to"] = relative_to
+        env = jinja2_env
+    return env
+
 def expandTemplatesInStr(s: str, args: Dict, env: Environment = None) -> str:
     """
     Expand Jinja2 templates in a string using the provided arguments.
@@ -17,14 +52,7 @@ def expandTemplatesInStr(s: str, args: Dict, env: Environment = None) -> str:
         The rendered string with templates expanded.
     """
 
-    global jinja2_env
-
-    if env is None:
-        if jinja2_env is None:
-            jinja2_env = Environment()
-        env = jinja2_env
-
-    return env.from_string(s).render(args)
+    return setupJinja2Env(env).from_string(s).render(args)
 
 def expandTemplates(o: Any, args: Dict, env: Environment = None) -> Any:
     """
