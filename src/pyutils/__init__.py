f414374f3d3402551ed0792dc610b68e0121ce9f
mirekfoo <mirekfoo@users.noreply.github.com>
2026-01-25 14:59:09 +0100

    bump version: 0.1.5 → 0.2.0

--- src/pyutils/__init__.py
+++ src/pyutils/__init__.py
@@ -1,7 +1,7 @@
 """Python Utilities Collection."""
 
 # For acceptable version formats, see https://www.python.org/dev/peps/pep-0440/
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 
 # from . import config_util
 # from . import file_util
